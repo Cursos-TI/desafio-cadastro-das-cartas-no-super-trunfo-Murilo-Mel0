71cae6eeb5d53ea4cb66622dd6072ef72ab1eb99
Murilo-Mel0 <Melomurilo2020@Gmail.com>
2025-03-04 16:36:43 -0300

    Update CartasSuperTrunfoMuriloMelo.c

--- CartasSuperTrunfoMuriloMelo.C
+++ CartasSuperTrunfoMuriloMelo.C
@@ -6,10 +6,31 @@ int main (){
     float Área;
     float PIB;
     char Nome;
-    char Codigo da Carta;
+    char Codigo da carta;
     char Estado;
 
     printf("Estado: \n");
-    scanf("%d", &Estado
+    scanf("%d", &Estado);
+
+    printf("Codigo da carta: \n");
+    scanf("%d", &Codigo da carta);
+
+    printf("Nome: \n");
+    scanf("%d", &Nome);
+
+    printf("População: \n");
+    scanf("%d", &População);
+
+    printf("Área: \n");
+    scanf("%d", &Área);
+
+    printf("PIB: \n");
+    scanf("%d", &PIB);
+
+    printf("Número de pontos turisticos: \n");
+    scanf("%d", &Número de pontos turisticos);
+
+    Printf("Estado: %s - Codigo da carta: %d", Estado, Codigo da carta);
+    Printf("Nome: %s - População: %f", Nome, População);
 
 }
